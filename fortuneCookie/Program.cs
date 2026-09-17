dd413895a1a4f2da6624aba811dd5c2aede88df2
jmpau <jmpau@JMP_WORK>
2026-09-17 08:39:44 -0600

    Add project files.

---
+++ fortuneCookie/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace fortuneCookie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new();
+            int choice = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                choice = rnd.Next(1, 7);
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine($"{choice} is 1");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{choice} is 2");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{choice} is 3");
+                        break;
+                    case 4:
+                        Console.WriteLine($"{choice} is 4");
+                        break;
+                    case 5:
+                        Console.WriteLine($"{choice} is 5");
+                        break;
+                    case 6:
+                        Console.WriteLine($"{choice} is 6");
+                        break;
+                }
+            }
+            Console.WriteLine("All done");
+            //pause
+            Console.ReadLine();
+        }
+    }
+}
